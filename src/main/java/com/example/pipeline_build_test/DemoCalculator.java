ba6145e68d0b9bf28f1e79cf75363532ed067cd4
akash41111 <104068921+akash41111@users.noreply.github.com>
2024-03-21 15:38:34 +0530

    modify the code to run the test case successfully

--- src/main/java/com/example/pipeline_build_test/DemoCalculator.java
+++ src/main/java/com/example/pipeline_build_test/DemoCalculator.java
@@ -3,11 +3,11 @@ package com.example.pipeline_build_test;
 public class DemoCalculator {
 	
 	public int add(int num1 , int num2) {
-		return num1 * num2 ;
+		return num1 + num2 ;
 	}
 	
 	public int multiply(int num1 , int num2) {
-		return num1 + num2;
+		return num1 * num2;
 	}
 
 
